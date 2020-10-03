1c2f4d1e9c678187d3c8f26f3d7130f2add755e7
Tsypchenko Yulia <yutsypa@icloud.com>
2020-10-03 18:22:34 +0300

    added models

---
+++ Domain/EF_Models/Sign.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.EF_Models
+{
+    public class Sign
+    {
+        public Guid Id { get; set; }
+        public User Manager { get; set; }
+        public int UserId { get; set; }
+        public bool Approved { get; set; }
+        public Application Application { get; set; }
+        public int ApplicationId { get; set; }
+    }
+}
---
+++ Domain/EF_Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.EF_Models
+{
+    public class User//:IdentityUser
+    {
+        public User()
+        {
+            Applications = new List<Application>();
+            Signs = new List<Sign>();
+        }
+        public string Position { get; set; }
+        public ICollection<Application> Applications { get; set; }
+        public ICollection<Sign> Signs { get; set; }
+    }
+}
