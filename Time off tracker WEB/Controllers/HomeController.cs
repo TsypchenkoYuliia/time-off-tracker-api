26dbafeea427bf6ec53236bf1a6c080553bb6aef
PedenkoIllia <pedenkoia@gmail.com>
2020-10-04 21:49:33 +0300

    Create WEB

---
+++ Time off tracker WEB/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Time_off_tracker_WEB.Models;
+
+namespace Time_off_tracker_WEB.Controllers
+{
+    [ApiController]
+    [Route("/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public TestViewModel Get()
+        {
+            TestViewModel model = new TestViewModel();
+            model.Smth = "Test";
+
+            return model;
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+
+        }
+    }
+}
---
+++ Time off tracker WEB/Models/TestViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Time_off_tracker_WEB.Models
+{
+    public class TestViewModel
+    {
+        public string Smth { get; set; }
+    }
+}
